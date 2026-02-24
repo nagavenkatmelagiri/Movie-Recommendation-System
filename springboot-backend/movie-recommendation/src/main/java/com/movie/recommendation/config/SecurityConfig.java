8a1b1808af9e46f051603122c4f77d42f43cb62e
nagavenkatmelagiri <nagavenkat205@gmail.com>
2026-02-24 12:28:42 +0530

    ..

--- springboot-backend/movie-recommendation/src/main/java/com/movie/recommendation/config/SecurityConfig.java
+++ springboot-backend/movie-recommendation/src/main/java/com/movie/recommendation/config/SecurityConfig.java
@@ -1,17 +1,17 @@
 package com.movie.recommendation.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
